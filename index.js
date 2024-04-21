0e32d5e6d02bb9298b5e32a7b82321f4a53714a8
shoptorshi <shoptorshiria78@gmail.com>
2024-04-21 22:23:51 +0600

    setup with mongodb also with 2 post api

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7bvfsss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+
+    const incomeCollections = client.db("MoneyTracker").collection("income");
+    const expenseCollections = client.db("MoneyTracker").collection("expense");
+
+    // post income
+    app.post('/income', async (req, res) => {
+
+        try {
+          const income = req.body;
+          const result = await incomeCollections.insertOne(income)
+          res.send(result);
+        }
+        catch (error) {
+          console.log(error)
+        }
+      })
+    //   post expense
+    app.post('/expense', async (req, res) => {
+
+        try {
+          const expense = req.body;
+          const result = await expenseCollections.insertOne(expense)
+          res.send(result);
+        }
+        catch (error) {
+          console.log(error)
+        }
+      })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res)=>{
+    res.send('server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
