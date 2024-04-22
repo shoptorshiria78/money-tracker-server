29c87725af7417b1335befc078db664dcb64c548
shoptorshi <shoptorshiria78@gmail.com>
2024-04-22 11:38:22 +0600

    expense delete api is made

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7bvfsss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,54 +38,69 @@ async function run() {
     // post income
     app.post('/income', async (req, res) => {
 
-        try {
-          const income = req.body;
-          const result = await incomeCollections.insertOne(income)
-          res.send(result);
-        }
-        catch (error) {
-          console.log(error)
-        }
-      })
+      try {
+        const income = req.body;
+        const result = await incomeCollections.insertOne(income)
+        res.send(result);
+      }
+      catch (error) {
+        console.log(error)
+      }
+    })
     //   post expense
     app.post('/expense', async (req, res) => {
 
-        try {
-          const expense = req.body;
-          const result = await expenseCollections.insertOne(expense)
-          res.send(result);
-        }
-        catch (error) {
-          console.log(error)
-        }
-      })
+      try {
+        const expense = req.body;
+        const result = await expenseCollections.insertOne(expense)
+        res.send(result);
+      }
+      catch (error) {
+        console.log(error)
+      }
+    })
     //   get income
 
-    app.get('/getIncome/:email', async(req, res)=>{
-        try{
-            const email = req.params.email;
-            const query = { email: email}
-            const result = await incomeCollections.find(query).toArray();
-            res.send(result);
+    app.get('/getIncome/:email', async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { email: email }
+        const result = await incomeCollections.find(query).toArray();
+        res.send(result);
 
-        }
-        catch(error){
-            console.log(error)
-        }
+      }
+      catch (error) {
+        console.log(error)
+      }
     })
     //  get expense
 
-    app.get('/getExpense/:email', async(req, res)=>{
-        try{
-            const email = req.params.email;
-            const query = { email: email}
-            const result = await expenseCollections.find(query).toArray();
-            res.send(result);
+    app.get('/getExpense/:email', async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { email: email }
+        const result = await expenseCollections.find(query).toArray();
+        res.send(result);
 
-        }
-        catch(error){
-            console.log(error)
-        }
+      }
+      catch (error) {
+        console.log(error)
+      }
+    })
+
+    // delete expense
+
+    app.delete('/deleteExpense/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await expenseCollections.deleteOne(query);
+        res.send(result)
+
+      }
+      catch (error) {
+        console.log(error)
+      }
     })
 
 
@@ -99,10 +114,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('server is running')
+app.get('/', (req, res) => {
+  res.send('server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
 })
